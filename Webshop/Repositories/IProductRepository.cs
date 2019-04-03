91a1f9c1390a7d1a0e2c77db879b48705443de75
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-04-03 12:45:16 +0200

    IProductRepository added

--- Webshop/Repositories/IProductRepository.cs
+++ Webshop/Repositories/IProductRepository.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using Webshop.Models;
+
 namespace Webshop.Repositories
 {
     public interface IProductRepository
     {
+        List<Product> Get();
+        Product Get(int id);
+        void Add(Product product);
+        void Delete(int id);
     }
 }
