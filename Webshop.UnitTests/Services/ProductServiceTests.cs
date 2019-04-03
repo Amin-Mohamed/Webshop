6692e7182abe63850b55ea21aa32792af6b7c499
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-04-03 12:38:04 +0200

    product service test added

--- Webshop.UnitTests/Services/ProductServiceTests.cs
+++ Webshop.UnitTests/Services/ProductServiceTests.cs
@@ -1,8 +1,44 @@
-﻿
+﻿using NUnit.Framework;
+using Webshop.Models;
+using Webshop.Services;
+using FakeItEasy;
+using Webshop.Repositories;
+
 namespace Webshop.UnitTests.Services
 {
     public class ProductServiceTests
     {
+        private IProductRepository productRepository;
+        private ProductService productService;
 
+        [SetUp]
+        public void SetUp()
+        {
+            this.productRepository = A.Fake<IProductRepository>();
+            this.productService = new ProductService(productRepository);
+        }
+
+        [Test]
+        public void Get_GivenId_ReturnsResultFromRepository()
+        {
+            // Arrange
+            const int Id = 20;
+            var productItem = new Product
+            {
+                Id = Id,
+                Title = "car",
+                Description = "BWM",
+                Price = 1300,
+                Image = null
+            };
+
+            A.CallTo(() => this.productRepository.Get(Id)).Returns(productItem);
+
+            // Act
+            var result = this.productService.Get(Id);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(productItem));
+        }
     }
 }
---
+++ Webshop/Repositories/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Webshop.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+}
